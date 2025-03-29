d52a3e47eaf23950d15d484c224147293092c4bd
Mateo974Run <974mateo@gmail.com>
2025-03-29 20:49:29 +0100

    Create confirmation_paiement.php

---
+++ phase2/confirmation_paiement.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>Votre paiement a été effectué avec succès !</h2>";
+echo "<p>Merci pour votre réservation. Vous recevrez bientôt un e-mail de confirmation.</p>";
+?>
+
+<a href="index.php">Retour à la page d'accueil</a>
