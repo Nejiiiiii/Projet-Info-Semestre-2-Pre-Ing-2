0f936e72dfd54d111af2ef9cbfee3f5840700220
Mateo974Run <974mateo@gmail.com>
2025-03-29 20:49:55 +0100

    Create erreur_paiement.php

---
+++ phase2/erreur_paiement.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>Le paiement a échoué.</h2>";
+echo "<p>Les coordonnées bancaires fournies ne sont pas valides. Veuillez réessayer ou modifier vos options.</p>";
+?>
+
+<a href="page_paiement.php">Retour à la page de paiement</a>
